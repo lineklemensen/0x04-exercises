4c8a89aca3ba323bb93729213eaf8cff09483fdc
Line Klemensen <202407497@post.au.dk>
2024-11-12 14:47:12 +0100

    FIrst attempt on the findMax function, and implementation of prototype

--- src/exercise-0x00/findMax.cpp
+++ src/exercise-0x00/findMax.cpp
@@ -1,1 +1,12 @@
 #include "findMax.h"
+
+int findMax(int a, int b, int c) {
+    if (a > b && a > c) {
+        return a;
+    } else if (b > a && b > c) {
+        return b;
+    } else if (c> a && c> b) {
+        return c;
+    }
+    
+}
--- src/exercise-0x00/findMax.h
+++ src/exercise-0x00/findMax.h
@@ -1,6 +1,6 @@
 #ifndef findMax_h
 #define findMax_h
 
-int findMax(int a, int b);
+int findMax(int a, int b, int c);
 
 #endif // findMax_h
