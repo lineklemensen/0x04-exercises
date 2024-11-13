81ac887ae6ae4e81b317c5e7cebe2332283dd7df
Line Klemensen <202407497@post.au.dk>
2024-11-13 13:50:57 +0100

    First attempt and added variable types.

--- src/exercise-0x01/calculateFactorial.cpp
+++ src/exercise-0x01/calculateFactorial.cpp
@@ -1,2 +1,10 @@
 #include "calculateFactorial.h"
 
+int calculateFactorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * calculateFactorial(n - 1);
+    }
+    
+}
--- src/exercise-0x01/main.cpp
+++ src/exercise-0x01/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "calculateFactorial.h"
+
+using namespace std;
+
 
 int main() {
-  std::cout << "Exercise 0x01" << std::endl;
+  int n;
+
+  //Ask the user to enter a number for the factorial
+  cout << "Please enter af number: ";
+  cin >> n;
+
+  //Calculate the factorial of the number
+  int factorial = calculateFactorial(n);
+
+  //Print the result
+  cout << "The factoral of the number " << n << " is: " << factorial << endl;
+  
   return 0;
 }
