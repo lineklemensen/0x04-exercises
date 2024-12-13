4519442a961461fb0f2792dae32eb8c4df0825fd
Line Klemensen <202407497@post.au.dk>
2024-12-13 09:21:22 +0100

    Definition and prototype of the area function for calculation of the area of a circle. Using the cmath library.

--- src/exercise-0x02/circleArea.cpp
+++ src/exercise-0x02/circleArea.cpp
@@ -1,1 +1,6 @@
 #include "circleArea.h"
+#include <cmath>
+
+double area(double radius) {
+    return M_PI * pow(radius, 2);
+}
--- src/exercise-0x02/circleArea.h
+++ src/exercise-0x02/circleArea.h
@@ -1,6 +1,6 @@
 #ifndef CIIRCLE_AREA_H
 #define CIIRCLE_AREA_H
 
-
+double area(double radius);
 
 #endif // CIIRCLE_AREA_H
