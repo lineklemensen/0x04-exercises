d9b348aa897cb5a1c868bb67cc1db77cb706757c
Line Klemensen <202407497@post.au.dk>
2024-12-14 16:41:24 +0100

    Initial work

--- src/exercise-0x03/main.cpp
+++ src/exercise-0x03/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "calculatePower.h"
 
 int main() {
   std::cout << "Exercise 0x03" << std::endl;
--- src/exercise-0x04/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "Exercise 0x04" << std::endl;
-  return 0;
-}
