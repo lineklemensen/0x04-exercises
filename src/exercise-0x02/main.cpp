f85c7b481ea20c491a9e100996adbcac1b704146
Line Klemensen <202407497@post.au.dk>
2024-12-14 16:22:32 +0100

    Testing the functions without taking user input.

--- src/exercise-0x02/main.cpp
+++ src/exercise-0x02/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "circleArea.h"
+#include "rectangleArea.h"
+#include "triangleArea.h"
+
+using namespace std;
 
 int main() {
-  std::cout << "Exercise 0x02" << std::endl;
+  //Testing the area function without user input
+  cout << "Area of the circle with input (radius = 7)" << area(7) << endl;
+  cout << "Area of the rectangle with inout (width = 5, height = 10)" << area(5, 10) << endl;
+  cout << "Area of the triangle with inout (a = 3, b = 4, c = 5)" << area(3, 4, 5) << endl;
+  
   return 0;
 }
