cd20fdfbf5117bdfc604eba13f95750501505325
Line Klemensen <202407497@post.au.dk>
2024-12-14 17:02:47 +0100

    Run and done!!!

--- src/exercise-0x03/main.cpp
+++ src/exercise-0x03/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include "calculatePower.h"
 
+using namespace std;
+
 int main() {
-  std::cout << "Exercise 0x03" << std::endl;
+  //Variables containing the values of the circuit
+  double v, r;
+
+  //Asking the user to input values for the circuit elements
+  cout << "Please input the values of the circuit elements" << endl;
+  cout << "Voltage V=[V]: ";
+  cin >> v;
+  cout << "Resistance R=[Ohm]: ";
+  cin >> r;
+
+  //Variable containing the result, i.e. the power dissipated in the resistor
+  double power = calculatePower(v, r);
+
+  //Outputting the result of the calculation to the user
+  cout << "The power dissipated in the resistor is " << power << " watts [W]" << endl;
+
   return 0;
 }
