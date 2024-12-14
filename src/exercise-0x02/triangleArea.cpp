db09db1047d199fe091bb05e4c81d80f1c56d37a
Line Klemensen <202407497@post.au.dk>
2024-12-14 16:06:38 +0100

    Definition and prototype of the area function for the triangle.

--- src/exercise-0x02/triangleArea.cpp
+++ src/exercise-0x02/triangleArea.cpp
@@ -1,1 +1,7 @@
 #include "triangleArea.h"
+#include <cmath>
+
+double area(double a, double b, double c) {
+    double s = (a + b + c) / 2;
+    return sqrt(s * (s - a) * (s - b) * (s - c));
+}
--- src/exercise-0x02/triangleArea.h
+++ src/exercise-0x02/triangleArea.h
@@ -1,6 +1,6 @@
 #ifndef TRIANGLE_AREA_H
 #define TRIANGLE_AREA_H 
 
-
+double area(double a, double b, double c);
 
 #endif // TRIANGLE_AREA_H
